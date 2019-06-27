4d2fe069c00adbc33f528d4897e8fdf33d6221a9
Harun Rashid <mdharunbd88@gmail.com>
2019-06-27 14:31:49 +0600

    Another changed

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+function persoanl_theme_setup(){
+	add_theme_support('title-tags');
+}
+
+
+add_action('after_setup_theme', 'persoanl_theme_setup');
+
+
+
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 	<!DOCTYPE html>
-	<html lang="zxx" class="no-js">
+	<html <?php language_attributes(); ?>>
 	<head>
 		<!-- Mobile Specific Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
+		
 		<!-- Author Meta -->
 		<meta name="author" content="colorlib">
 		<!-- Meta Description -->
@@ -13,8 +12,7 @@
 		<meta name="keywords" content="">
 		<!-- meta character set -->
 		<meta charset="UTF-8">
-		<!-- Site Title -->
-		<title>Personal</title>
+		
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
 			<!--
